6fd2951a063c4cb8a0c0d344877535466853623e
dana <157533000+Dana11a@users.noreply.github.com>
2026-04-08 11:41:56 +0300

    TODO

--- 6-4-express-request-data/server.js
+++ 6-4-express-request-data/server.js
@@ -111,12 +111,10 @@ import express from "express"
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
+const PORT = 3000;
 
-app.listen(3000, () => {
-  console.log("API running at http://localhost:3000");
+app.listen(PORT, () => {
+   console.log(`API running at http://localhost:${PORT}`);
 });
 
 // Query params: /echo?name=Ali&age=22
